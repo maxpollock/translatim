4d0c0a8f52b1504093796b314d5a377363aab750
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-02 16:40:26 +0000

    added img and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [from, setFrom] = useState("ar");
-  const [to, setTo] = useState("ar");
+  const [from, setFrom] = useState("en");
+  const [to, setTo] = useState("es");
   const [word, setWord] = useState("");
   const [translation, setTranslation] = useState("");
   const [img, setImg] = useState("");
@@ -22,35 +22,47 @@ function App() {
     <>
       <div className="titles">
         <h1>my translatim</h1>
-        <h2>where gifs and language come together</h2>
+        <h2>where language and gifs come together</h2>
       </div>
       <form onSubmit={handleTranslate}>
         <div className="container">
           <select onChange={(event) => setFrom(event.target.value)}>
-            <option value="ar">Arabic</option>
             <option value="en">English</option>
+            <option value="ar">Arabic</option>
+
             <option value="pl">Polish</option>
             <option value="es">Spanish</option>
             <option value="tr">Turkish</option>
           </select>
           <input
-            placeholder="Translate"
+            placeholder="Enter words here..."
             onChange={(event) => setWord(event.target.value)}
           />
         </div>
         <div className="container">
           <select onChange={(event) => setTo(event.target.value)}>
+            <option value="es">Spanish</option>
             <option value="ar">Arabic</option>
             <option value="en">English</option>
             <option value="pl">Polish</option>
-            <option value="es">Spanish</option>
+
             <option value="tr">Turkish</option>
           </select>
-          <input value={translation} readOnly placeholder="Translation" />
+          <input
+            value={translation}
+            readOnly
+            placeholder="Translation will appear here"
+          />
         </div>
-        <button>Translate</button>
+        <button>Translate & Generate Gif</button>
+
+        {img && (
+          <div id="gif-title">
+            <h2>Your Gif:</h2>
+            <img src={img} />
+          </div>
+        )}
       </form>
-      <img src={img} />
     </>
   );
 }
