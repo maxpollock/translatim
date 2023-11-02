34ef7d9b29f7cf3ee0ac64292bae5ccfa38a3bdf
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-02 09:57:16 +0000

    git gud

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(cors());
+const axios = require("axios");
+// add your endpoints here
+
+app.get(PORT, (_, response) => response.json("Root route for translatim."));
+
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
