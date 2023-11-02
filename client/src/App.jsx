2e13c1218d23a785a4210242db94c631b3a7b45c
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-02 15:40:41 +0000

    added gifs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,55 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <></>;
+  const [from, setFrom] = useState("ar");
+  const [to, setTo] = useState("ar");
+  const [word, setWord] = useState("");
+  const [translation, setTranslation] = useState("");
+  const [img, setImg] = useState("");
+
+  // onsubmit function that calls the API to get the translation
+  async function handleTranslate(event) {
+    event.preventDefault();
+    const API = `http://localhost:8080/translate?word=${word}&from=${from}&to=${to}`;
+    const res = await axios.get(API);
+  
+    setTranslation(res.data.translation);
+    setImg(res.data.image);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleTranslate}>
+        <div className="container">
+          <select onChange={(event) => setFrom(event.target.value)}>
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="pl">Polish</option>
+            <option value="es">Spanish</option>
+            <option value="tr">Turkish</option>
+          </select>
+          <input
+            placeholder="Translate"
+            onChange={(event) => setWord(event.target.value)}
+          />
+        </div>
+        <div className="container">
+          <select onChange={(event) => setTo(event.target.value)}>
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="pl">Polish</option>
+            <option value="es">Spanish</option>
+            <option value="tr">Turkish</option>
+          </select>
+          <input value={translation} readOnly placeholder="Translation"/>
+        </div>
+        <button>Translate</button>
+      </form>
+      <img src={img} />
+    </>
+  );
 }
 
 export default App;
