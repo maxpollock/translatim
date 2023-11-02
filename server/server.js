807a16c6e8d921571c27d38ca840d4a745b9b976
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-02 11:47:48 +0000

    changed client and server. Hooked up backend to work with api

--- server/server.js
+++ server/server.js
@@ -5,8 +5,23 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 const axios = require("axios");
-// add your endpoints here
 
 app.get("/", (_, response) => response.json("Root route for translatim."));
+
+app.get("/translate", async (request, response) => {
+  // const word = request.query.word
+  // const from = request.query.from
+  // const to = request.query.to
+
+  const { word, from, to } = request.query;
+
+  const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+  const res = await axios.get(API);
+
+  response.json({
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  });
+});
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
